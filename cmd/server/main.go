fce5c77cbe781d37ac03716ce8e15c25a3832c08
Hoang Phuc Nguyen <jphuc96@gmail.com>
2019-06-29 03:07:40 +0700

    add mongodb

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,34 +1,64 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 	"path/filepath"
 
 	"github.com/k0kubun/pp"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
+	// CONNECT K8S
 	// Use cli
-	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-	config, err := clientcmd.BuildConfigFromFlags("https://ops.sudojoss.com:6443", kubeconfig)
+	kubeconfig := filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "jphuc96", "dev8s", "secret", "config")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// Use rest api
 	// config := &rest.Config{}
-	clientSet, err := kubernetes.NewForConfig(config)
+	k8sClient, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	pods, err := clientSet.CoreV1().Pods("default").List(metav1.ListOptions{})
+	// CONNECT MONGO
+	clientOptions := options.Client().ApplyURI("mongodb://root:dev8s@localhost:27017")
+	MongoClient, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	pp.Println(pods)
+	// Check the connection
+	err = MongoClient.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection := MongoClient.Database("dev8s_test").Collection("numbers")
+	res, err := collection.InsertOne(context.Background(), bson.M{"name": "pi", "value": 3.14})
+	pp.Println("Inserted", res.InsertedID)
+
+	pods, err := k8sClient.CoreV1().Pods("default").List(metav1.ListOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, v := range pods.Items {
+		pp.Println(v.Name)
+	}
+	// podsJSON, err := json.MarshalIndent(pods, "", " ")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// ioutil.WriteFile("pods.json", podsJSON, 0644)
 }
